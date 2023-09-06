057228252bd14be761ef798e53dc29bcda3aaa44
Hicham BAKIR <hicham@bakir.ch>
2023-09-06 16:32:54 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Hicham Bakir on 06.09.23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
