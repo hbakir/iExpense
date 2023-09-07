1996680e5fcbf5c6bfeb4057b9fd695312cbccb6
Hicham BAKIR <hicham@bakir.ch>
2023-09-07 12:36:17 +0200

    Add data and forms to handle expenses list

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Hicham Bakir on 07.09.23.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
