b7d28b2fd361c59092c4cd9d8ebcc9788e95eede
Hicham BAKIR <hicham@bakir.ch>
2023-09-08 14:45:27 +0200

    Split busines and personal expenses

---
+++ iExpense/ExpenseSection.swift
@@ -0,0 +1,40 @@
+//
+//  ExpenseSection.swift
+//  iExpense
+//
+//  Created by Hicham Bakir on 08.09.23.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    let title: String
+    let expenses: [ExpenseItem]
+    let deleteItems: (IndexSet) -> Void
+
+    var body: some View {
+        Section(title) {
+            ForEach(expenses) { item in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        Text(item.type)
+                    }
+
+                    Spacer()
+
+                    Text(item.amount, format: .currency(code: item.currency))
+                        .style(for: item)
+                }
+            }
+            .onDelete(perform: deleteItems)
+        }
+    }
+}
+
+struct ExpenseSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseSection(title: "Example", expenses: [ExpenseItem](), deleteItems: { _ in })
+    }
+}
